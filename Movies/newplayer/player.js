f42bf305d5ca3d914cd8a031bfc0804585194efe
Priyanshu Kumar Singh <147051156+PriyanshuSingh120@users.noreply.github.com>
2026-02-01 03:02:53 +0530

    Update player.js

--- Movies/newplayer/player.js
+++ Movies/newplayer/player.js
@@ -173,6 +173,14 @@ const movieDatabase = {
         "tmdb": "66732",
         "episodes": ["https://short.icu/RrBB6-79l","https://short.icu/YcwrZOAyz","https://short.icu/U8oxvfuW2","https://short.icu/pBvuStnhl"]
     },
+    "rabbit-trap": {
+  "title": "Rabbit Trap",
+  "img": "https://image.tmdb.org/t/p/w500/sKnwnnmBpjo9BEskHtja6ToXUV1.jpg",
+  "year": "2025",
+  "tmdb": "1193824",
+  "type": "movie",
+  "src": "https://short.icu/yRPvo363E"
+},
     "sultan": { "title": "Sultan", "src": "https://short.icu/t-H9cuHzO", "year": "2016", "img": "https://image.tmdb.org/t/p/w500/vPTYtKuV8X5uUd3SqnzO14W6Dti.jpg" },
     "lubber-pandhu": { "title": "Lubber Pandhu", "src": "https://short.icu/zX7mBJdLR", "year": "2024", "img": "https://image.tmdb.org/t/p/w500/jNyLZjIgaYVkBli2JihHwABlAPY.jpg" },
     "ujda-chaman": { "title": "Ujda Chaman", "src": "https://short.icu/sl5Rizeo4", "year": "2019", "img": "https://image.tmdb.org/t/p/w500/hybSFEHZcehPN8oRdb5YjaxRk2u.jpg" },
@@ -184,7 +192,7 @@ const movieDatabase = {
     "12th-fail": { "title": "12th Fail", "src": "https://short.icu/-tGxzoOLL", "year": "2023", "img": "https://image.tmdb.org/t/p/w500/eebUPRI4Z5e1Z7Hev4JZAwMIFkX.jpg" },
     "welcome-back": { "title": "Welcome Back", "src": "https://short.icu/72MTfaND8", "year": "2015", "img": "https://image.tmdb.org/t/p/w500/xnRIT3ggVcJxGWylKw85n6pteLp.jpg" },
     "cheekatilo": { "title": "Cheekatilo", "src": "https://short.icu/wwLdO_MZa3", "year": "2026", "img": "https://image.tmdb.org/t/p/w500/pO86EjZb7QyGYk5RW4Xm4UWe5Uv.jpg" },
-    "dhurandhar": { "title": "Dhurandhar", "src": "https://short.icu/9l2LoJVx0h", "year": "2025", "img": "https://image.tmdb.org/t/p/w500/8FHOtUpNIk5ZPEay2N2EY5lrxkv.jpg" },
+    "dhurandhar": { "title": "Dhurandhar", "src": "https://short.icu/rfFnG9g89", "year": "2025", "img": "https://image.tmdb.org/t/p/w500/8FHOtUpNIk5ZPEay2N2EY5lrxkv.jpg" },
     "777-charlie": { "title": "777 Charlie", "src": "https://short.icu/rDBkCtcHEf", "year": "2022", "img": "https://image.tmdb.org/t/p/w500/qArPmXH0aWsT3SEtYl8XrU2Oz48.jpg" },
     "border-2": { "title": "Border 2", "src": "https://short.icu/s2mCKLYkm", "year": "2026", "img": "https://image.tmdb.org/t/p/w500/q7ow2BdAeQIc6zVteJ0x1Uxmixt.jpg" },
     "tere-ishk-mein": { "title": "Tere Ishk Mein", "src": "https://short.icu/TYhQXfBmF", "year": "2025", "img": "https://image.tmdb.org/t/p/w500/7FsHlW9z7gDhDE4EXAE2WcXUVQj.jpg" },
@@ -385,6 +393,7 @@ function generateRecommendations(currentId) {
         grid.appendChild(card);
     });
 }
+
 
 
 
