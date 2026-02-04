b536c28ec72492702b2b34d2b5c6cc1c751d676e
Priyanshu Kumar Singh <147051156+PriyanshuSingh120@users.noreply.github.com>
2026-02-04 21:43:07 +0530

    Update player.js

--- Movies/newplayer/player.js
+++ Movies/newplayer/player.js
@@ -205,6 +205,14 @@ const movieDatabase = {
   "type": "movie",
   "src": "https://short.icu/xz3Vdlj8K"
 },
+    "animal": {
+  "title": "Animal",
+  "img": "https://image.tmdb.org/t/p/w500/hr9rjR3J0xBBKmlJ4n3gHId9ccx.jpg",
+  "year": "2023",
+  "tmdb": "781732",
+  "type": "movie",
+  "src": "https://short.icu/8fq5gE5D9"
+},
     "sultan": { "title": "Sultan", "src": "https://short.icu/t-H9cuHzO", "year": "2016", "img": "https://image.tmdb.org/t/p/w500/vPTYtKuV8X5uUd3SqnzO14W6Dti.jpg" },
     "lubber-pandhu": { "title": "Lubber Pandhu", "src": "https://short.icu/zX7mBJdLR", "year": "2024", "img": "https://image.tmdb.org/t/p/w500/jNyLZjIgaYVkBli2JihHwABlAPY.jpg" },
     "ujda-chaman": { "title": "Ujda Chaman", "src": "https://short.icu/sl5Rizeo4", "year": "2019", "img": "https://image.tmdb.org/t/p/w500/hybSFEHZcehPN8oRdb5YjaxRk2u.jpg" },
@@ -417,6 +425,7 @@ function generateRecommendations(currentId) {
         grid.appendChild(card);
     });
 }
+
 
 
 
